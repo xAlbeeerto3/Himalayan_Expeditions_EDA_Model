da36d465d831ce74b0825a472d74f04e449db48c
Alberto <albertolopezesteve00@gmail.com>
2024-07-18 23:36:53 +0200

    Update App_Himalaya.py

--- Proyecto_final_Himalaya/App_Himalaya.py
+++ Proyecto_final_Himalaya/App_Himalaya.py
@@ -52,7 +52,7 @@ st.markdown(
     unsafe_allow_html=True)
 
 # Sidebar para el menú principal
-logos_expediciones = (r'Proyecto_final_Himalaya\Imagenes\logos_expediciones.png')
+logos_expediciones = (r'C:\Users\Alberto\Documents\GitHub\Himalayan_Expeditions_EDA_Model\Proyecto_final_Himalaya\Imagenes\7expedicionistas.jpg')
 st.sidebar.image(logos_expediciones, width=250)
 st.sidebar.header('Opciones', divider='rainbow')
 st.sidebar.markdown(
